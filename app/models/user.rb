1db3082f64378297f72adb1e328b95b2633718ff
ikaruga <ikaruga777@gmail.com>
2018-08-02 11:04:02 +0900

    SlackIDを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :lunches, dependent: :destroy
+  validates :slack_id, presence: true
   validates :name, presence: true
   validates :admin, presence: true
 end
