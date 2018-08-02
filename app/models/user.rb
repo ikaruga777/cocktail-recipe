cb461c04a56f2caf57d7c1ebedc8b7c4a1f45339
ikaruga <ikaruga777@gmail.com>
2018-08-02 17:27:42 +0900

    slack_idは必須としない
    inviteする際にslack登録が前提になってしまうため

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :lunches, dependent: :destroy
-  validates :slack_id, presence: true
   validates :name, presence: true
   validates :admin, presence: true
 end
--- db/migrate/20180730084653_create_users.rb
+++ db/migrate/20180730084653_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :slack_id, null: false
+      t.string :slack_id
       t.boolean :admin, default: false, null: false
 
       t.timestamps
