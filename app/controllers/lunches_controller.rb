f42dcb648a2c8cce641ae786c7d459190c488f8a
ikaruga <ikaruga777@gmail.com>
2018-08-06 18:40:54 +0900

    Lunch#showの実装
    ランチの詳細情報が見れる

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -4,6 +4,10 @@ class LunchesController < ApplicationController
     @lunches = Lunch.all
   end
 
+  def show
+    @lunch = Lunch.find_by(id: params[:id])
+  end
+  
   def new
     @lunches = Lunch.new
   end
