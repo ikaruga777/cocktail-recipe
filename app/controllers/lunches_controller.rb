a18b4e3a6d0a2135a57bc8bf54b1b88c4f3b91d4
ikaruga <ikaruga777@gmail.com>
2018-08-09 12:17:49 +0900

    ランチの予定を登録する
    シンプルな実装

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -8,4 +8,22 @@ class LunchesController < ApplicationController
     @lunch = Lunch.find_by(id: params[:id])
   end
 
+  def new
+    @lunch = Lunch.new
+  end
+
+  def create
+    @lunch = Lunch.new
+    user = User.find_by(name: params[:lunch][:user])
+    @lunch.user = user
+    # TODO: lunch_paramsでうまくやる方法をみつける
+    @lunch.scheduled_for = params[:lunch][:scheduled_for]
+    @lunch.place = params[:lunch][:place]
+    @lunch.state = :scheduled
+    if @lunch.save
+      redirect_to lunches_path
+    else
+      render 'new'
+    end
+  end
 end
