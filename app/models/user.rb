bd1c087864b96a5a5e36bcad2616f45b72d7207d
ikaruga <ikaruga777@gmail.com>
2018-10-17 20:34:32 +0900

    デストロイです

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :attendances, dependent: :destory
+  has_many :attendances, dependent: :destroy
   has_many :lunches, through: :attendances
   validates :name, presence: true
   validates :admin, inclusion: {in: [true,false]}
